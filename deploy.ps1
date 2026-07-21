4f1ffac08d9c89bc620cbe4bafecfc08fdcaf74f
Lance Espina <lanceespina478@gmail.com>
2026-07-21 23:46:32 +0800

    minor

--- deploy.ps1
+++ deploy.ps1
@@ -7,7 +7,7 @@ Write-Host "Publishing API..." -ForegroundColor Cyan
 dotnet publish "$root\EcaInformationSystem.API\EcaInformationSystem.API.csproj" -c Release -o "$root\publish\api"
 
 Write-Host "Deploying API to MonsterASP..." -ForegroundColor Cyan
-& $msdeploy -verb:sync -source:contentPath="$root\publish\api" -dest:contentPath=site76298,ComputerName="https://site76298.siteasp.net:8172/msdeploy.axd?site=site76298",UserName=site76298,Password=REDACTED,AuthType=Basic -allowUntrusted
+& $msdeploy -verb:sync -source:contentPath="$root\publish\api" -dest:contentPath=site76298,ComputerName="https://site76298.siteasp.net:8172/msdeploy.axd?site=site76298",UserName=site76298,Password=REDACTED,AuthType=Basic -allowUntrusted -enableRule:AppOffline
 
 Write-Host "Publishing Client..." -ForegroundColor Cyan
 dotnet publish "$root\EcaInformationSystem.Client\EcaInformationSystem.Client.csproj" -c Release -o "$root\publish\client"
